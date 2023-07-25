b620d848e57f7ad4704c8000fba94dfb78bcda6b
itsalfie729 <139905194+itsalfie729@users.noreply.github.com>
2023-07-25 23:54:06 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://fitnessici.xyz/graphql"
+	const endpoint = "https://skillsproo.blogspot.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-				 `https://fitnessici.xyz/` + encodeURI(path as string)
+				 `https://skillsproo.blogspot.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
